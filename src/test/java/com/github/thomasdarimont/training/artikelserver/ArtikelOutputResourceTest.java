645b062e1dfd2599f67bc21ebdefcab0fd147f60
Thomas Darimont <thomas.darimont@googlemail.com>
2023-04-28 21:27:30 +0200

    Add example for testing with rest assured

--- src/test/java/com/github/thomasdarimont/training/artikelserver/ArtikelOutputResourceTest.java
+++ src/test/java/com/github/thomasdarimont/training/artikelserver/ArtikelOutputResourceTest.java
@@ -1,21 +1,28 @@
 package com.github.thomasdarimont.training.artikelserver;
 
+import com.github.thomasdarimont.training.artikelserver.sortiment.rest.ArtikelOutput;
 import io.quarkus.test.junit.QuarkusTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class ArtikelOutputResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
-        given()
-                .when().get("/hello")
+    public void shouldReturnArtikelById() {
+
+        var artikelOutput = given()
+                .when().get("/api/sortiment/artikel/1")
                 .then()
                 .statusCode(200)
-                .body(is("Hello RESTEasy"));
+                .extract()
+                .as(ArtikelOutput.class);
+
+        assertThat(artikelOutput).hasFieldOrPropertyWithValue("ean", "EAN1");
     }
 
 }
